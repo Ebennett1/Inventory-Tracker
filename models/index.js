6593937fb980d07b46e5431e2abb10fd4c5c603b
Ebennett1 <evontebennett@gmail.com>
2024-03-02 14:38:04 -0800

    Connected to MongoDB database

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGODBURI);
+const db = mongoose.connection
+	
+db.on('connected', function() {
+  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
